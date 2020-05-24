5dff47237d9e705c40ed38843ef2cd955630a921
chengzeyi <ichengzeyi@gmail.com>
2020-05-24 14:04:51 +0800

    finish cgroups

--- cgroups/cgroup_manager.go
+++ cgroups/cgroup_manager.go
@@ -1,5 +1,7 @@
 package cgroups
 
+import "fmt"
+import log "github.com/sirupsen/logrus"
 
 type CgroupManager struct {
 	Path string // The path of this cgroup in the overall hierarchy.
@@ -13,16 +15,40 @@ func NewCgroupManager(path string) *CgroupManager {
 
 // Add process of pid to this cgroup.
 func (c *CgroupManager) Apply(pid int) error {
-	return nil
+	var retErr error
+	for _, subsystem := range subsystems {
+		if err := subsystem.Apply(c.Path, pid); err != nil {
+			retErr = fmt.Errorf("Apply() cgroup %s to pid %d error %v", c.Path, pid, err)
+			log.Error(retErr)
+		}
+	}
+
+	return retErr
 }
 
 // Set resource limitations of this cgroup.
-func (c *CgroupManager) Set(pid int) error {
-	return nil
+func (c *CgroupManager) Set(res *ResourceConfig) error {
+	var retErr error
+	for _, subsystem := range subsystems {
+		if err := subsystem.Set(c.Path, res); err != nil {
+			retErr = fmt.Errorf("Set() cgroup %s error %v", c.Path, err)
+			log.Error(retErr)
+		}
+	}
+
+	return retErr
 }
 
 // Release this cgroup.
-func (c *CgroupManager) Destroy(pid int) error {
-	return nil
+func (c *CgroupManager) Destroy() error {
+	var retErr error
+	for _, subsystem := range subsystems {
+		if err := subsystem.Remove(c.Path); err != nil {
+			retErr = fmt.Errorf("Remove() cgroup %s error %v", c.Path, err)
+			log.Error(retErr)
+		}
+	}
+
+	return retErr
 }
 
--- cgroups/subsystem.go
+++ cgroups/subsystem.go
@@ -1,9 +1,25 @@
 package cgroups
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/chengzeyi/dicker/util"
+)
+
 type ResourceConfig struct {
 	MemoryLimit string
-	CpuShare    string
-	CpuSet      string
+	CpuShares   string
+	Cpuset      string
+}
+
+var subsystems = []Subsystem{
+	&CpuSubsystem{},
+	&CpusetSubsystem{},
+	&MemorySubsystem{},
 }
 
 type Subsystem interface {
@@ -15,21 +31,86 @@ type Subsystem interface {
 
 type SubsystemBase struct {}
 
-type CpuSubsystem struct {}
-
-func (CpuSubsystem) Name() string {
-
+type CpuSubsystem struct {
+	SubsystemBase
 }
 
-func (CpuSubsystem) Set(path string, res *ResourceConfig) error {
-	panic("not implemented") // TODO: Implement
+type CpusetSubsystem struct {
+	SubsystemBase
 }
 
-func (CpuSubsystem) Apply(path string, pid int) error {
-	panic("not implemented") // TODO: Implement
+type MemorySubsystem struct {
+	SubsystemBase
 }
 
-func (CpuSubsystem) Remove(path string) error {
-	panic("not implemented") // TODO: Implement
+func (s *SubsystemBase) Name() string {
+	panic("not implemented")
 }
 
+func (s *SubsystemBase) set(path, key, val string) error {
+	if len(val) == 0 {
+		return nil
+	}
+
+	cgroupPath, err := util.GetCgroupPath(s.Name(), path, true)
+	if err != nil {
+		return fmt.Errorf("GetCgroupPath() error %v", err)
+	}
+	
+	filePath := filepath.Join(cgroupPath, key)
+	if err := ioutil.WriteFile(filePath, []byte(val), 0644); err != nil {
+		return fmt.Errorf("WriteFile() %s error %v", filePath, err)
+	}
+
+	return nil
+}
+
+func (s *SubsystemBase) Apply(path string, pid int) error {
+	cgroupPath, err := util.GetCgroupPath(s.Name(), path, false)
+	if err != nil {
+		return fmt.Errorf("GetCgroupPath() error %v", err)
+	}
+
+	tasksFilePath := filepath.Join(cgroupPath, "tasks")
+	if err := ioutil.WriteFile(tasksFilePath, []byte(strconv.Itoa(pid)), 0644); err != nil {
+		return fmt.Errorf("WriteFile %s error %v", tasksFilePath, err)
+	}
+
+	return nil
+}
+
+func (s *SubsystemBase) Remove(path string) error {
+	cgroupPath, err := util.GetCgroupPath(s.Name(), path, false)
+	if err != nil {
+		return fmt.Errorf("GetCgroupPath() error %v", err)
+	}
+	if err := os.RemoveAll(cgroupPath); err != nil {
+		return fmt.Errorf("RemoveAll() %s error %v", cgroupPath, err)
+	}
+
+	return nil
+}
+
+func (s *CpuSubsystem) Name() string { 
+	return "cpu"
+}
+
+func (s *CpuSubsystem) Set(path string, res *ResourceConfig) error {
+	return s.set(path, "cpu.shares", res.CpuShares)
+}
+
+func (s *CpusetSubsystem) Name() string { 
+	return "cpuset"
+}
+
+func (s *CpusetSubsystem) Set(path string, res *ResourceConfig) error {
+	return s.set(path, "cpuset.cpus", res.Cpuset)
+}
+
+func (s *MemorySubsystem) Name() string { 
+	return "memory"
+}
+
+func (s *MemorySubsystem) Set(path string, res *ResourceConfig) error {
+	return s.set(path, "memory.limit_in_bytes", res.MemoryLimit)
+}
